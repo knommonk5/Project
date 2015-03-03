d7f9f9ae800f77056e6431a02c4a5602586c3ff8
knommonk5 <knommonk5_stian@hotmail.com>
2015-03-03 18:47:20 +0100

    Oppdatering

--- Oblig oving 3/src/KeyCardSystem.java
+++ Oblig oving 3/src/KeyCardSystem.java
@@ -1,4 +1,35 @@
 
 public class KeyCardSystem {
-
+	
+	private static int cards;
+	private String fNavn;
+	private String sNavn;
+	
+	
+	public void Kort(String fName, String sName, int pin){
+		
+		
+		
+	}
+	
+	public void setFNavn(){
+		this.fNavn = fNavn;
+	}
+	public String getSNavn(){
+		return fNavn;
+	}
+	public void setNavn(){
+		this.sNavn = sNavn;
+	}
+	public String getNavn(){
+		return sNavn;
+	}
+	
+	public boolean isSperret(){
+		return false;
+	}
+	
+	public toString(){
+		return String;
+	}
 }
