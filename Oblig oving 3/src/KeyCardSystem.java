a1ff9981795377dd9873d0855aeff20e832235af
knommonk5 <knommonk5_stian@hotmail.com>
2015-03-05 17:43:12 +0100

    v02

--- Oblig oving 3/src/KeyCardSystem.java
+++ Oblig oving 3/src/KeyCardSystem.java
@@ -1,35 +1,81 @@
 
-public class KeyCardSystem {
+public abstract class KeyCardSystem {
 	
 	private static int cards;
 	private String fNavn;
 	private String sNavn;
 	
+	private int PIN, kortNummer;
 	
-	public void Kort(String fName, String sName, int pin){
-		
-		
+	private boolean sperret;
+	
+	public void Kort(String fName, String sName, int PIN){
+		this.fNavn = forNavn;
+		this.sNavn = etterNavn;
+		this.PIN = PIN;
+		kortNummer = ++cards;
 		
 	}
 	
-	public void setFNavn(){
-		this.fNavn = fNavn;
-	}
-	public String getSNavn(){
-		return fNavn;
-	}
-	public void setNavn(){
-		this.sNavn = sNavn;
-	}
-	public String getNavn(){
-		return sNavn;
-	}
+	public abstract boolean sjekkPIN(int pin);
+	
+	
 	
 	public boolean isSperret(){
-		return false;
+		return sperret;
 	}
 	
-	public toString(){
-		return String;
+	public String getNavn() {
+		return forNavn + " " + etterNavn;
+	}
+
+	public boolean erSperret() {
+		return sperret;
+	}
+
+	public void endreSperret() {
+		sperret = !sperret;
+	}
+
+	public static int getKort() {
+		return cards;
+	}
+
+	public String getForNavn() {
+		return fNavn;
+	}
+
+	public void setForNavn(String forNavn) {
+		this.fNavn = forNavn;
+	}
+
+	public String getEtterNavn() {
+		return sNavn;
+	}
+
+	public void setEtterName(String etterNavn) {
+		this.sNavn = etterNavn;
+	}
+
+	public int getPIN() {
+		return PIN;
+	}
+
+	public int getKortNummer() {
+		return kortNummer;
+	}
+	
+	public static void setKort(int kortNummer) {
+		cards = kortNummer;
+	}
+
+	public boolean getSperret() {
+		return sperret;
+	}
+
+	public String toString() {
+		return String.format(
+				"Navn:\t\t%s\nPIN:\t\t%d\nKort nummer:\t%d\nEr sperret:\t%b\n",
+				getNavn(), PIN, kortNummer, sperret);
 	}
 }
