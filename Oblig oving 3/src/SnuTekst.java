961b0f783a7c55a838e3a8eda115d0275b8acd6e
knommonk5 <knommonk5_stian@hotmail.com>
2015-03-05 20:44:32 +0100

    v5-2

--- Oblig oving 3/src/SnuTekst.java
+++ Oblig oving 3/src/SnuTekst.java
@@ -1,20 +1,27 @@
 import java.util.Scanner;
 
-public class SnuTekst {
+public class SnuTekst1 {
 
 	private static Scanner in = new Scanner(System.in);
 
-	public static void main(String[] args) {
-		System.out.println("Skriv inn det du vil snu");
-		String userInput = in.nextLine();
+	private static int antall = 0;
 
-		baklengs(userInput);
+	public static void main(String[] args) {
+		System.out
+				.println("Skriv inn det du vil snu");
+		String tekst = in.nextLine();
+
+		baklengs(tekst);
 	}
 
 	public static void baklengs(String tekst) {
 		if (tekst.length() > 0) {
 			System.out.print(tekst.charAt(tekst.length() - 1));
-			baklengs(tekst.substring(0, tekst.length()-1));
+			antall++;
+			baklengs(tekst.substring(0, tekst.length() - 1));
+		} else {
+			System.out.println("Det er totalt" + antall + " ord.");
+			antall = 0;
 		}
 	}
 }
